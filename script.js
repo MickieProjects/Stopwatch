bcd916cadf7201d122f955ca725e1f0d86396544
Khathathon Nissayan <143895455+MickieProjects@users.noreply.github.com>
2023-11-05 11:23:15 +0700

    update JS

--- script.js
+++ script.js
@@ -7,11 +7,62 @@ let stopTime = true
 
 // Start Timer
 function startTimer() {
-    if (startTimer == true) {
+    if (stopTime == true) {
         stopTime = false
         timeCycle()
     }
 }
 
 // Stop Timer
-fun
+function stopTimer() {
+    stopTime == false && (stopTime = true)
+}
+
+function timeCycle() {
+    if (stopTime == false) {
+        hr = parseInt(hr)
+        min = parseInt(min)
+        sec = parseInt(sec)
+
+        //Counting Seconds (timeInterval)
+        sec += 1
+
+        // Limitting to 60 seconds
+        if (sec == 60) {
+            min += 1
+            sec = 0
+        }
+
+        // Limiting to 60 minutes
+        if (min == 60) {
+            hr += 1
+            min = 0
+            sec = 0
+        }
+
+        // Adding zero "0"
+        if (sec < 10) {
+            sec = '0' + sec
+        }
+        if (min < 10) {
+            min = '0' + min
+        }
+        if (hr < 10) {
+            hr = '0' + hr
+        }
+
+        // Display stopwatch
+        time.innerHTML = `${hr}:${min}:${sec}`
+
+        setTimeout('timeCycle()', 1000)
+    }
+}
+
+// Reset Timer
+function resetTimer() {
+    time.innerHTML = '00:00:00'
+    stoptime = true
+    hr = 0
+    sec = 0
+    min = 0
+}
